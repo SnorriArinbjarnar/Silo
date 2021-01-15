a478fe9099faca495a60dace3df573cdd620071b
SnorriArinbjarnar <snorriarinbjarnar@gmail.com>
2021-01-15 20:25:11 +0000

    fixed bootstrap button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,53 +1,74 @@
-import React from 'react'
+import React, {Component} from 'react'
 //import {Link, NavLink} from 'react-router';
 
-function Navbar() {
+class Navbar extends Component {
+  constructor(props){
+    super(props);
+    this.toogleNavbar = this.toogleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toogleNavbar(){
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
+
+  render(){
+    const collapsed = this.state.collapsed;
+    const classOne = collapsed ? 'collapse navbar-collapse' : 'collapse navbar-collapse show';
+    const classTwo = collapsed ? 'navbar-toggler navbar-toggler-right collapsed' : 'navbar-toggler navbar-toggler-right';
     return (
-<nav class="navbar navbar-expand-md navbar-light bg-white sticky-top ">
-      <div class="container-fluid">
-        <a href="/#" class="navbar-brand">
-          LOGO
-        </a>
-        <div class="navigation-part ">
-            <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarResponsive"
-            >
-            <span class="navbar-toggler-icon"></span>
-            </button>
-
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a href="/#" class="nav-link">Heim</a>
-            </li>
-            <li class="nav-item">
-              <a href="#about" class="nav-link">Um okkur</a>
-            </li>
-            <li class="nav-item">
-              <a href="#services" class="nav-link">Þjónusta</a>
-            </li>
-            
-            <li class="nav-item">
-              <a href="#contact" class="nav-link">Hafðu Samband</a>
-            </li>
-            
-          </ul>
-        </div>
-      </div>
-      <div class="phone-part text-primary p-2">
-
-        <span >555-4567</span>
-        <i class="fa fa-phone" aria-hidden="true"></i>
-
-        
-      </div>
-    </div>
+      <nav class="navbar navbar-expand-md navbar-light bg-white sticky-top ">
+            <div class="container-fluid">
+              <a href="/#" class="navbar-brand">
+                LOGO
+              </a>
+              <div class="navigation-part ">
+                  <button
+                  className={`${classTwo}`}
+                  type="button"
+                  data-toggle="collapse"
+                  data-target="#navbarResponsive"
+                  onClick={this.toogleNavbar}
+                  >
+                  <span class="navbar-toggler-icon"></span>
+                  </button>
     
-    </nav>
-    )
+              <div className={`${classOne}`} id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                  <li class="nav-item active">
+                    <a href="/#" class="nav-link">Heim</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#about" class="nav-link">Um okkur</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="#services" class="nav-link">Þjónusta</a>
+                  </li>
+                  
+                  <li class="nav-item">
+                    <a href="#contact" class="nav-link">Hafðu Samband</a>
+                  </li>
+                  
+                </ul>
+              </div>
+            </div>
+            <div class="phone-part text-primary p-2">
+    
+              <span >555-4567</span>
+              <i class="fa fa-phone" aria-hidden="true"></i>
+    
+              
+            </div>
+          </div>
+          
+          </nav>
+          )
+  }
+    
 }
 
 export default Navbar
